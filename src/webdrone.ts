337406bb85c50b0a2cc96630c426d232199a8379
Viktor Sandström <sandstrom.viktor@gmail.com>
2023-01-07 11:46:15 +0100

    motion

--- src/webdrone.ts
+++ src/webdrone.ts
@@ -3,6 +3,7 @@ let gps: HTMLElement | null = document.getElementById("getLocation");
 let start: HTMLElement | null = document.getElementById("startLocation");
 let contGps: HTMLElement | null = document.getElementById("getContinuousLocation");
 let container: HTMLElement | null = document.getElementById("container");
+let motion: HTMLElement | null = document.getElementById("motion");
 
 const url: string = "http://127.0.0.1:5000";
 
@@ -16,8 +17,17 @@ const url: string = "http://127.0.0.1:5000";
 // })
 
 if (window.DeviceMotionEvent) {
-    window.addEventListener("deviceorientation", (e) => handleOrientation(e));
+    window.addEventListener("devicemotion", (e) => handleMotion(e));
 
+}
+
+const handleMotion = (e: DeviceMotionEvent) => {
+    motion.innerHTML = 
+    `
+    x: ${e.rotationRate.alpha}
+    y: ${e.rotationRate.beta}
+    z: ${e.rotationRate.gamma}
+    `
 }
 
 

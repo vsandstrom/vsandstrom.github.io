1c4ebaab361e1db8b29fbdccc6d8cc7ed05b09e0
Viktor Sandström <sandstrom.viktor@gmail.com>
2023-01-09 00:38:47 +0100

    sound

--- src/webdrone.js
+++ src/webdrone.js
@@ -13,11 +13,21 @@ const state = document.getElementById("status");
 
 const FUND = 200;
 
-let vol = new Tone.Volume(0.5).toDestination();
-let verb = new Tone.Reverb(4).chain(vol);
-let fm0 = new Tone.FMSynth().chain(verb);
-let fm1 = new Tone.FMSynth().chain(verb);
-let fm2 = new Tone.FMSynth().chain(verb);
+let pan0 = new Tone.Panner3D(0,0,0).toDestination();
+let pan1 = new Tone.Panner3D(0,0,0).toDestination();
+let pan2 = new Tone.Panner3D(0,0,0).toDestination();
+
+let vol0 = new Tone.Volume(0.5).chain(pan0);
+let vol1 = new Tone.Volume(0.5).chain(pan1);
+let vol2 = new Tone.Volume(0.5).chain(pan2);
+
+let verb0 = new Tone.Reverb(4).chain(vol0);
+let verb1 = new Tone.Reverb(4).chain(vol1);
+let verb2 = new Tone.Reverb(4).chain(vol2);
+
+let fm0 = new Tone.FMSynth({type: "fmtriangle"}).chain(verb);
+let fm1 = new Tone.FMSynth({type: "fmtriangle"}).chain(verb);
+let fm2 = new Tone.FMSynth({type: "fmtriangle"}).chain(verb);
 // fm0.triggerAttackRelease("C2", "8n");
 
 let accx, accy, accz = 0.0;
@@ -38,15 +48,14 @@ const handleMotion = (e) => {
     `;
 
 
-
     fm0.set({
-        frequency: FUND + accx
+        frequency: (200 + accx)
     });
     fm1.set({
-        frequency: (FUND * 5/4 )- accx
+        frequency: ((200 * (5/4) )- accx)
     });
     fm2.set({
-        frequency: (FUND * 9/8 )- accx
+        frequency: ((200 * (9/8) )- accx)
     });
 
 
@@ -64,6 +73,16 @@ const handleOrientation = (e) => {
         y: ${Math.floor(oriy)}<br>
         z: ${Math.floor(oriz)}<br>
     `;
+
+    pan0.set({
+        positionX: orix,
+        positionY: oriy
+    });
+    pan1.set({
+        positionX: 360 - orix,
+        positionY: oriy
+    });
+
 };
 
 // drone.addEventListener('click', async () => {

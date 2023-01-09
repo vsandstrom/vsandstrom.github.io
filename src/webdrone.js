fac554ec79eaab6424621e6f63d2f9b54b2bbee4
Viktor Sandström <sandstrom.viktor@gmail.com>
2023-01-09 16:17:15 +0100

    3synths

--- src/webdrone.js
+++ src/webdrone.js
@@ -78,15 +78,18 @@ const toneStart = async () => {
 
     let part0 = new Tone.Part(((time, note) => {
         fm0.triggerAttackRelease(note, "5", time);
-    }), [ [0, FUND * 7], ["0:6", FUND*6], ["0:14", FUND*5]]).start(0);
+    }), [ [0, FUND * 7], ["0:6", FUND*6], ["0:14", FUND*5]]).loop(true)
+    part0.start(0);
 
     let part1 = new Tone.Part(((time, note) => {
         fm1.triggerAttackRelease(note, "5", time);
-    }), [ ["0:2:5", FUND*7], ["0:7", FUND*6], ["0:13:5", FUND*3]]).start(0);
+    }), [ ["0:2:5", FUND*7], ["0:7", FUND*6], ["0:13:5", FUND*3]]).loop(true)
+    part1.start(0);
     
     let part2 = new Tone.Part(((time, note) => {
         fm2.triggerAttackRelease(note, "5", time);
-    }), [ ["0:2:5", FUND*1.125], ["0:7", FUND*5/3], ["0:13:5", FUND*5/4] ]).start(0);
+    }), [ ["0:2:5", FUND*1.125], ["0:7", FUND*5/3], ["0:13:5", FUND*5/4] ]).loop(true)
+    part2.start(0);
 
     Tone.Transport.start();
 

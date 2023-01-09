0e44a500114ab2c0247514ed9aaae448040406ea
Viktor Sandström <sandstrom.viktor@gmail.com>
2023-01-09 07:03:02 +0100

    3synths

--- src/webdrone.js
+++ src/webdrone.js
@@ -13,11 +13,22 @@ const state = document.getElementById("status");
 
 const FUND = 200;
 
-let vol = new Tone.Volume(1).toDestination();
-let verb = new Tone.Reverb(2).chain(vol);
-let fm0 = new Tone.FMSynth().chain(verb);
-let fm1 = new Tone.FMSynth().chain(verb);
-let fm2 = new Tone.FMSynth().chain(verb);
+let pan0 = new Tone.Panner3D({panningModel: "HRTF", positionX: 0}).toDestination();
+let pan1 = new Tone.Panner3D({panningModel: "HRTF", positionX: 120}).toDestination();
+let pan2 = new Tone.Panner3D({panningModel: "HRTF", positionX: 240}).toDestination();
+
+let vol0 = new Tone.Volume(1).toDestination();
+let vol1 = new Tone.Volume(1).toDestination();
+let vol2 = new Tone.Volume(1).toDestination();
+
+let verb0 = new Tone.Reverb(2).chain(vol0);
+let verb1 = new Tone.Reverb(2).chain(vol1);
+let verb2 = new Tone.Reverb(2).chain(vol2);
+
+
+let fm0 = new Tone.FMSynth().chain(pan0);
+let fm1 = new Tone.FMSynth().chain(pan1);
+let fm2 = new Tone.FMSynth().chain(pan2);
 
 
 let accx, accy, accz = 0.0;
@@ -28,8 +39,8 @@ const handleOrientation = (e) => {
 
     let inc = 1 / 360;
 
-    orix = Math.abs(e.alpha) * inc;
-    oriy = Math.abs(e.beta) * inc;
+    orix = (e.alpha) * inc;
+    oriy =  e.beta * inc;
     oriz = Math.abs(e.gamma) * inc;
 
     orient.innerHTML = `
@@ -65,6 +76,13 @@ const handleMotion = (e) => {
     fm0.set({ frequency: FUND + accx });
     fm1.set({ frequency: (FUND * 1.125) + accy });
     fm2.set({ frequency: (FUND * 1.25) - accx });
+
+    pan0pos = pan0.get().positionX;
+    pan0.set({positionX: pan0pos + accx});
+    pan1pos = pan1.get().positionX;
+    pan1.set({positionX: pan1pos + accx});
+    pan2pos = pan2.get().positionX;
+    pan2.set({positionX: pan2pos + accx});
 };
 
 
